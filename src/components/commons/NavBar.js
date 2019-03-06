62c0fb69863b16f0b0d2563591e11dcbe20e02fe
tesh254 <ewachira254@gmail.com>
2019-03-06 11:47:14 +0300

    feat(Navbar)
    - Created Navbar component
    - Made the Navbar responsive
    
    [Feature #164422763]

---
+++ src/components/commons/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import myFunction from "../../assets/js/main";
+import mascot from "../../assets/svg/mascot.svg";
+
+const Navbar = () => (
+  <div className="topnav" id="myTopnav">
+    <NavLink to="/" id="link" className="active">
+        <img src={mascot} alt="mascot-logo" className="logo"/>
+    </NavLink>
+    <NavLink to="/about" id="link" className="right-link">
+      About
+    </NavLink>
+    <a href="javascript:void(0);" className="icon right-link" onClick={myFunction}>
+      <i className="fa fa-bars" />
+    </a>
+  </div>
+);
+
+export default Navbar;
