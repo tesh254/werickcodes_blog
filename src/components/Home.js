16077d6655aa42b594158eeecf5e4d0cfa2662cf
tesh254 <ewachira254@gmail.com>
2019-05-12 19:29:16 +0300

    feat(metas): preview
    - change the meta tags according to the view
    - also prreviews should appear
    
    [Finishes #165957295]

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 // import mascot from "../assets/svg/mascot.svg";
 import Articles from "./containers/Articles";
+import Metas from "../components/commons/MetaTags";
+import mascot from "../assets/img/img.png";
 
 const Home = () => (
   <div>
+    <Metas 
+      title="Werick Codes"
+      description="Easy tutorials to kickoff your Software Developer life"
+      image={mascot}
+    />
     <p className="domain">Werick Blog</p>
     <Articles />
   </div>
--- src/components/commons/Contact.js
+++ src/components/commons/Contact.js
@@ -1,9 +1,16 @@
 /* eslint-disable */
 
 import React from "react";
+import Metas from "../commons/MetaTags";
+import mascot from "../../assets/img/img.png";
 
 const ContactForm = () => (
   <div>
+    <Metas
+      title="Contact Werick"
+      description="Get in touch with Werick Codes"
+      image={mascot}
+    />
     <form action="https://formspree.io/ewachira254@gmail.com" className="form-contact" method="POST">
       <h1>Contact Form</h1>
       <input type="text" placeholder="Enter your full name" name="name" /><br/><br/>
