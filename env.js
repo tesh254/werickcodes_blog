55b2167fa5a0dc707ee44e52be5e095bbeba3317
tesh254 <ewachira254@gmail.com>
2019-08-29 12:10:30 +0300

    Fix for bad gateway

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export default {
-    base_url: "https://api.werick.tk/api/v1",
+    base_url: "http://api.werick.codes/api/v1",
     production_url: "https://werick.codes",
     disqus_shortname: "werick",
     disqus_url: "werick.tk"
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { parsed: localEnv } = require("dotenv").config();
+// const { parsed: localEnv } = require("dotenv").config();
 
 // module.exports = {
 //   webpack: config => {
